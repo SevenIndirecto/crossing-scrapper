b2c7cb78975fd476110ed899dec829773a69ce43
sevenindirecto <zabkar@gmail.com>
2021-08-05 01:55:23 +0200

    Add daily stats to visualizer

--- scrapper-visualizer/src/App.vue
+++ scrapper-visualizer/src/App.vue
@@ -1,15 +1,42 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <p>
       Source: <a href="https://www.hak.hr/info/stanje-na-cestama/?lang=en#border-crossings">hak.hr</a>
     </p>
+    <div>
+      <label for="select-day">Show Stats for Day of Week:</label>
+      <select id="select-day" v-model="selectedDay" @change="displayDailyGraphs">
+        <option v-for="[dayOfWeek, dayName] of days" :key="dayOfWeek" :value="dayOfWeek">{{ dayName }}</option>
+      </select>
+    </div>
+    <div v-show="selectedDay === 0 || selectedDay">
+      <button @click.prevent="hideAll('dayChartTo')">Hide All</button>
+      <button @click.prevent="showAll('dayChartTo')">Show All</button>
+      <highcharts :options="dayChartToCroatiaOptions" :callback="afterDayChartToCreate" />
+      <hr>
+      <button @click.prevent="hideAll('dayChartFrom')">Hide All</button>
+      <button @click.prevent="showAll('dayChartFrom')">Show All</button>
+      <highcharts :options="dayChartFromCroatiaOptions" :callback="afterDayChartFromCreate" />
+    </div>
+    <hr>
+    <h2>Historical Data</h2>
+    <div>
+      <button @click.prevent="hideAll('chartTo')">Hide All</button>
+      <button @click.prevent="showAll('chartTo')">Show All</button>
+    </div>
     <highcharts :options="chartToCroatiaOptions" :callback="afterChartToCreate"></highcharts>
+    <hr>
+    <div>
+      <button @click.prevent="hideAll('chartFrom')">Hide All</button>
+      <button @click.prevent="showAll('chartFrom')">Show All</button>
+    </div>
     <highcharts :options="chartFromCroatiaOptions" :callback="afterChartFromCreate"></highcharts>
   </div>
 </template>
 
 <script>
 import { Chart } from 'highcharts-vue';
+import { fromUnixTime, getDay, getHours } from 'date-fns';
 
 let history = {};
 const crossingCoordinates = {};
@@ -75,7 +102,27 @@ const chartOptions = {
       },
     }
   },
-}
+};
+
+const dayChartOptions = {
+  xAxis: {
+    title: {
+      text: 'Hour of Day'
+    }
+  },
+  yAxis: {
+    title: {
+      text: 'Avg Wait Time (min)',
+    },
+  },
+  plotOptions: {
+    series: {
+      label: {
+        connectorAllowed: false
+      },
+    }
+  },
+};
 
 export default {
   name: 'App',
@@ -84,13 +131,27 @@ export default {
   },
   data() {
     return {
+      seriesByDayAndHour: null,
+      dayChartTo: null,
+      dayChartFrom: null,
       chartTo: null,
       chartFrom: null,
+      days: [
+        [1, 'Monday'],
+        [2, 'Tuesday'],
+        [3, 'Wednesday'],
+        [4, 'Thursday'],
+        [5, 'Friday'],
+        [6, 'Saturday'],
+        [0, 'Sunday'],
+      ],
+      selectedDay: '',
       ignoredCrossings: [
         'Jasenovac', 'Duboševica', 'Goričan II', 'Bajakovo', 'Pasjak',
         'Stara Gradiška', 'Županja', 'Slavonski Brod', 'Maljevac', 'Slavonski Šamac',
         'Tovarnik', 'Kamensko', 'Vinjani Gornji', 'Dvor', 'Goričan', 'Aržano',
-        'Zaton Doli',
+        'Zaton Doli', 'Ličko Petrovo Selo', 'Metković', 'Nova Sela',
+          'Vinjani Donji', 'Hrvatska Kostajnica',
       ],
       chartToCroatiaOptions: {
         ...chartOptions,
@@ -104,15 +165,98 @@ export default {
           text: 'Leaving Croatia',
         }
       },
+      dayChartToCroatiaOptions: {
+        ...dayChartOptions,
+        title: {
+          text: 'Entering Croatia on Selected Day',
+        }
+      },
+      dayChartFromCroatiaOptions: {
+        ...dayChartOptions,
+        title: {
+          text: 'Leaving Croatia on Selected Day',
+        }
+      },
     };
   },
   async created() {
     const url = process.env.NODE_ENV === 'development' ? '/history.sample.json' : '/history.json';
     const response = await fetch(url);
     history = await response.json();
+    this.buildStats();
     this.refreshCharts();
   },
   methods: {
+    displayDailyGraphs() {
+      this.displayDataForDay('from_cro', 'dayChartFrom');
+      this.displayDataForDay('to_cro', 'dayChartTo');
+    },
+    displayDataForDay(direction, targetChart) {
+      let key;
+      let series = {};
+      for (let { name, data } of Object.values(this.seriesByDayAndHour)) {
+        let aggregatedData = [];
+        for (let i = 0; i < 24; i++) {
+          key = `${this.selectedDay}_${i}`;
+
+          let avg;
+          if (key in data) {
+            avg = data[key][direction].reduce((acc, x) => acc + x, 0) / data[key][direction].length;
+          } else {
+            avg = 0;
+          }
+
+          aggregatedData.push([i, avg]);
+        }
+
+        series[name] = {
+          name,
+          data: aggregatedData,
+          avg: aggregatedData.reduce((acc, x) => acc + x[1], 0) / aggregatedData.length,
+          max: aggregatedData.reduce((acc, x) => Math.max(acc, x[1]), 0),
+        };
+        series[name].name = `${name} (avg. ${Math.floor(series[name].avg)}, max. ${series[name].max})`;
+      }
+
+      let seriesArray = Object.values(series);
+      seriesArray.sort((a, b) => a.avg - b.avg);
+      this[targetChart].update({ series: seriesArray }, true, true)
+    },
+    buildStats() {
+      let series = {};
+
+      for (let [time, crossings] of Object.entries(history)) {
+        if (!time || !crossings) {
+          continue;
+        }
+
+        for (let data of crossings) {
+          let crossing = data['crossing']?.trim();
+          if (!crossing || this.ignoredCrossings.includes(crossing)) {
+            continue;
+          }
+
+          if (!(crossing in series)) {
+            series[crossing] = {
+              name: crossing,
+              data: {},
+            }
+          }
+
+          let date = fromUnixTime(parseInt(time));
+          let dayOfWeek = getDay(date);
+          let hourOfDay = getHours(date);
+
+          let key = `${dayOfWeek}_${hourOfDay}`;
+          if (!(key in series[crossing]['data'])) {
+            series[crossing]['data'][key] = {'to_cro': [], 'from_cro': []};
+          }
+          series[crossing]['data'][key]['to_cro'].push(this.getMinutesFromDescription(data['to_cro']));
+          series[crossing]['data'][key]['from_cro'].push(this.getMinutesFromDescription(data['from_cro']));
+        }
+      }
+      this.seriesByDayAndHour = series;
+    },
     refreshCharts() {
       if (this.chartFrom && this.chartTo) {
         this.chartTo.update({
@@ -178,6 +322,16 @@ export default {
 
       return 0;
     },
+    hideAll(chart) {
+      for (let series of this[chart].series) {
+        series.hide();
+      }
+    },
+    showAll(chart) {
+      for (let series of this[chart].series) {
+        series.show();
+      }
+    },
     afterChartToCreate(chart) {
       this.chartTo = chart;
       this.chartTo.update({
@@ -190,17 +344,21 @@ export default {
         series: this.getChartData('from_cro')
       }, true, true);
     },
+    afterDayChartToCreate(chart) {
+      this.dayChartTo = chart;
+    },
+    afterDayChartFromCreate(chart) {
+      this.dayChartFrom = chart;
+    },
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 30px;
+}
+button {
+  margin-right: 0.5rem;
 }
 </style>
